47a58e9b1a918bddd9727484b6e8641a116c3da2
Karekin <445923692@qq.com>
2024-10-06 16:22:31 +0800

    doc(flink runtime)：学习Flink源码

--- flink-ademo/src/main/java/com/source/sql/demo/Demo.java
+++ flink-ademo/src/main/java/com/source/sql/demo/Demo.java
@@ -1,14 +1,17 @@
 package com.source.sql.demo;
 
+import com.source.sql.javacc.parser.select.ParseException;
+import com.source.sql.javacc.parser.select.SimpleSelectParser;
+
 public class Demo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         parseSelect("select 1+1");
         parseSelect("select 2-1");// select id ,name from table
     }
-    private static void parseSelect(String sql) {
-        //final SimpleSelectParser parser = new SimpleSelectParser(sql);
-        // 解析的核心方法
-        //parser.parse();
+    private static void parseSelect(String sql) throws ParseException {
+        final SimpleSelectParser parser = new SimpleSelectParser(sql);
+//        // 解析的核心方法
+        parser.parse();
     }
 
 }
